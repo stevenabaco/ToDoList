63d34f67f58b650bd6803327dcbbf7dad3b806a7
Steven Abaco <steve@wizardrywd.com>
2020-06-05 00:21:33 -0500

    complete basic functioning of todo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,27 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(e){
+$("ul").on("click", "span", (function(e){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   })
   e.stopPropagation();
-})
+}))
+
+$("input[type='text']").keypress(function(e){
+  if(e.which === 13){
+    //grab text entered in input field
+      var todoText = ($(this).val()); 
+    //clear out input field
+      $(this).val("");
+    //create a mew li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }    
+  });
+  
+
 
 
