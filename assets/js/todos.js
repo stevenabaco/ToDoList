a0bdc1f59e10a1f02b1b62ea320e04ae1a2021ec
Steven Abaco <steve@wizardrywd.com>
2020-06-05 16:05:49 -0500

    FInish stylinig

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -18,9 +18,16 @@ $("input[type='text']").keypress(function(e){
     //clear out input field
       $(this).val("");
     //create a mew li and add to ul
-    $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>");
+    $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>").show('slow');
     }    
   });
+
+  $(".fa-plus").click(function(){
+    $("input[type='text']").toggle('400', function(){
+      $("h1 i").toggleClass("fa-plus fa-minus");
+    });
+    
+  })
   
 
 
